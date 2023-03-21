01f37e3bfff8f8b73879ab1283e8f608b1a0b2d2
obkin <kapellwork@gmail.com>
2023-03-21 17:53:36 +0200

    lesson-51: user answers

--- index.js
+++ index.js
@@ -1,31 +1,19 @@
 import express from 'express';
 
-const port = 13000;
+const port = 3000;
 const app = express();
 
-app.all('/home', (req, res, next) => {
-    console.log('method all');
-    next();
-});
-
-const cb = (req, res, next) => {
-    console.log('Callback #2');
-    next();
-};
-
-app.get('/home', cb, (req, res) => {
-    res.send('Hello, main callback!')
-    console.log('Callback #1')
-});
-
-app.route('/user')
-    .get('/home', (req, res) => {
-        res.send('Home page.');
-    })
-    .post('/home', (req, res) => {
-        res.send('Post method for home page');
+app.get('/hello', (req, res) => {
+    res.cookie('name', 'value', {
+        domain: '',
+        path: '../',
+        secure: true,
+        expires: 600000
     });
+    res.clearCookie('name', { path: '../' });
+    res.send('Hello!');
+});
 
 app.listen(port, () => {
-    console.log(`The server started at http://localhost:${port}`);
+    console.log(`The server started at: http://localhost:${port}`);
 });
