f5e20624b8c90ea25858ed30651ff693f6f06a04
obkin <kapellwork@gmail.com>
2023-03-24 19:04:22 +0200

    lesson-56: rewrited to typescript

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import { userRouter } from './users/users.js';
 
 const port = 3000;
@@ -16,7 +16,7 @@ app.get('/hello', (req, res) => {
 
 app.use('/users', userRouter);
 
-app.use((err, req, res, next) => {
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     console.log(err.message);
     res.status(500).send(err.message);
 });
