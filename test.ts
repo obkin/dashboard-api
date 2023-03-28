125fca1d9059d336791af6922df5d5594a291c36
obkin <kapellwork@gmail.com>
2023-03-28 17:35:41 +0300

    lesson-59: type alises vs interfaces

--- test.ts
+++ test.ts
@@ -1,25 +1,67 @@
+type coords = { lat: number, long: number }; 
 
-let a: string | number = 5;
-    a = 'str';
+interface ICoords {
+    lat: number;
+    long: number;
+}
 
-// ---
+type ID = string | number;
 
-function printVar(arg: string | number) {
-    if (typeof arg === 'string') {
-        console.log(arg.toUpperCase());
-    } else {
-        console.log(arg + 1);
-    }
+type UniqueID = 'lost' | 'found';
+
+function compute(coords: ICoords) {
+
 }
 
 // ---------
 
-function helloUser(user: string | string[]) {
-    if (Array.isArray(user)) {
-        user.map(each => {
-            console.log(each + ', hello!');
-        });
-    } else {
-        user.toLowerCase();
-    }
+type myString = string;
+
+// ---------
+
+/*
+
+interface Animal {
+    name: string;
+}
+
+interface Dog extends Animal {
+    tail?: boolean
+}
+
+const dog: Dog = {
+    name: 'Jack',
+    // tail: true
+};
+
+*/
+
+// ---------
+
+type Animal = {
+    name: string;
+}
+
+type Dog = Animal & {
+    tail: boolean;
+}
+
+const dog: Dog = {
+    name: 'Maison',
+    tail: false
+}
+
+// ---------
+
+interface User {
+    name: string;
+}
+
+interface User {
+    age: number;
+}
+
+const user: User = {
+    name: 'Olya',
+    age: 21
 }
