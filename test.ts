3e7e8327a4a1cffa9b6e4baeee03eaf7dc47b733
obkin <kapellwork@gmail.com>
2023-03-28 13:04:53 +0300

    lesson-58: union types

--- test.ts
+++ test.ts
@@ -1,57 +1,25 @@
-const a: number = 5;
-const b: string = 'str';
-const d: boolean = true;
 
-let c: string = a + b + d;
-
-// ---------
-
-let arr: (string | number | boolean)[] = ['John', 'Walter', 'Kate', 1, false];
-
-let names: string[] = ['John', 'Walter', 'Kate'];
-let numbers: number[] = [1, 2, 3];
-let booleans: boolean[] = [true, true, false];
-
-let someArr: any[] = ['str', 1, true, {link: 'https://youtube.com'}]; // types is absent
-
-let tuple: [number, number, string, boolean, number] = [1, 5, 'str', false, 1];
-let tupleEx: [number, string, boolean, number] = [875, 'John', false, 28];
-
-// ---------
-
-function greet(name: string): string {
-    return name + ', hello!'; // John, hello!
-}
-
-console.log(names.map((name: string) => greet(name)));
-
-// ---------
-
-interface HelloFunc {
-    hello: {
-        hi: string;
-        name: string;
-    };
-}
-
-function helloFunc(obj: HelloFunc): string {
-    return obj.hello.hi + ', ' + obj.hello.name;
-}
+let a: string | number = 5;
+    a = 'str';
 
 // ---
 
-function hello(hello: { hi: string, name?: string }): string {
-    if (hello.name == 'string') {
-        return hello.hi + ',' + hello.name + '!';
+function printVar(arg: string | number) {
+    if (typeof arg === 'string') {
+        console.log(arg.toUpperCase());
     } else {
-        throw new Error(hello.name + ' - is undefined');
+        console.log(arg + 1);
     }
 }
 
-console.log(hello({hi: 'Hello', name: 'John'}));
-
 // ---------
 
-let f: any = 5;
-    f = 'str',
-    f = true;
+function helloUser(user: string | string[]) {
+    if (Array.isArray(user)) {
+        user.map(each => {
+            console.log(each + ', hello!');
+        });
+    } else {
+        user.toLowerCase();
+    }
+}
