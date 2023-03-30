de3aa02fb7acaf374009b7b9767914f5b36892a0
obkin <kapellwork@gmail.com>
2023-03-30 17:07:10 +0300

    lesson-61: enums

--- test.ts
+++ test.ts
@@ -1,68 +1,37 @@
-let a = 5;
-
-const b = 'str';
-
-let j: 'someStr' | 'str' | 5 = 'someStr';
-
-// ---------
-
 type directions = 'left' | 'right';
 
-function move(direction: directions): -1 | 1 | 0 {
+enum Directions {
+    Left,
+    Right
+}
+
+function move(direction: Directions) {
     switch (direction) {
-        case 'left':
+        case Directions.Left:
             return -1;
-            break;
-        case 'right':
+        case Directions.Right:
             return 1;
-            break;
-        default: 
+        default:
             return 0;
-            break;
     }
 }
 
-move('left');
-move('right');
+move(Directions.Left);
+move(Directions.Right);
 
 // ---------
 
-/*
-
-interface IConnection {
-    host: string;
-    port: number;
-}
-
-function connect(params: IConnection | 'default') {
-    if (typeof params == 'object') {
-        startConnection(params.host, params.port);
-    } else {
-        startConnection('localhost', 3000);
-    }
-}
-
-
-function startConnection(h: string, p: number) {
+function objMod(obj: { Left: number }) {
 
 }
 
-*/
+objMod(Directions);
 
 // ---------
 
-interface IParams {
-    host: string;
-    protocol: 'https' | 'http';
+const enum Directions2 {
+    Up,
+    Down
 }
 
-const connectionParams = {
-    host: 'localhost',
-    protocol: 'https' as 'https'
-}
-
-function connect(host: string, protocol: 'https' | 'http') {
-
-}
-
-connect(connectionParams.host, connectionParams.protocol);
+let myDirection = Directions2.Up;
