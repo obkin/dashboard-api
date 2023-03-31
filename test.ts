90ffee1246f6fb6ad768f078287195e208b1661d
obkin <kapellwork@gmail.com>
2023-03-31 11:26:44 +0300

    lesson-61: repeat

--- test.ts
+++ test.ts
@@ -1,37 +1,68 @@
-type directions = 'left' | 'right';
+type direction = 'left' | 'right';
 
 enum Directions {
-    Left,
-    Right
+    LEFT = 'left',
+    RIGHT = 'right',
+    TOP = 'top',
+    DOWN = 'down'
 }
 
 function move(direction: Directions) {
     switch (direction) {
-        case Directions.Left:
+        case Directions.DOWN:
             return -1;
-        case Directions.Right:
+        case Directions.LEFT:
+            return -1;
+        case Directions.RIGHT:
+            return 1;
+        case Directions.TOP:
             return 1;
         default:
             return 0;
     }
 }
 
-move(Directions.Left);
-move(Directions.Right);
+move(Directions.DOWN);
+move(Directions.LEFT);
+move(Directions.RIGHT);
+move(Directions.TOP);
 
 // ---------
 
-function objMod(obj: { Left: number }) {
+enum Host {
+    LOCALHOST = 'localhost',
+    HOST = 'myhost'
+}
+
+let a = 5;
+    a *= 5;
+
+enum Protocol {
+    HTTP = 'ast'.length,
+    HTTPS = Math.floor(0.9),
+    AKMS = a
+}
+
+function connect(host: Host, protocol: Protocol) {
 
 }
 
-objMod(Directions);
+connect(Host.LOCALHOST, Protocol.HTTP);
 
 // ---------
 
-const enum Directions2 {
-    Up,
-    Down
+const enum STATUS {
+    PAID = 600,
+    REJECTED,
+    REFUND
 }
 
-let myDirection = Directions2.Up;
+const b = STATUS.REFUND;
+
+// ---------
+
+function checkObj(obj: { LEFT: string }) {
+
+}
+
+checkObj(Directions);
