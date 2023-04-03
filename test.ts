a8ef4bf9882fff9c1a8e951e99b84b8bb332ad33
obkin <kapellwork@gmail.com>
2023-04-03 11:09:06 +0300

    lesson-62: repeat

--- test.ts
+++ test.ts
@@ -4,32 +4,34 @@ interface HasLength {
 
 function log<T1 extends HasLength, T2>(arg: T1, arr: T2[]): T2[] {
     arg.length
+    arr.length
 
     return arr;
 }
 
-log<HasLength, number>({ length: 5 }, [1, 5, 7]);
+log<HasLength, number>({ length: 9 }, [1, 3, 4]);
 
-// log<string, number>('str', [1, 5, 7]);
-// log<number, string>(5, ['str1', 'str2', 'str3']);
+// log<string, number>('str1', [1, 5, 6]);
+// log<number, string>(1, ['str1', 'str2']);
 // log<boolean, boolean>(true, [false, true, false]);
 
 // ---------
 
 interface User {
-    name: string;
+    name: string,
     age?: number;
+
     bid: <TYPE>(sum: TYPE) => boolean;
 }
 
-const obj: User = {
-    name: 'Jack',
-    age: 19,
-    bid: (sum) => {
+const user1: User = {
+    name: 'John',
+    age: 21,
+
+    bid: <TYPE>(sum: TYPE) => {
         return true;
     }
-}
+};
 
-obj.bid(12);
-obj.bid('12');
-obj.bid([1, 2, 4]);
+user1.bid<number>(100);
+user1.bid<string>('str');
