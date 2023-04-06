fa57246c04fe4db320d578e5189fe183bf7a4b09
obkin <kapellwork@gmail.com>
2023-04-06 15:09:55 +0300

    lesson-64: repeat

--- test.ts
+++ test.ts
@@ -1,39 +1,54 @@
-let a = 'John';
-
-console.log(typeof a);
-
-if (typeof a == 'string') {
-    //...
-} else {
-    //...
-}
-
-// ---
+let a = 'str';
 
 let b: typeof a;
+
+let d: typeof b;
+
+if (typeof a == 'string') {
+    console.log('string');
+}
 
 // ---------
 
 type Coord = {
-    lat: number;
-    long: number;
+    lat: number,
+    long: number
 }
 
-// enum Coord2 {
-//     lat = 'lat',
-//     long = 'long'
-// }
+interface ICoord {
+    lat: number,
+    long: number
+}
 
 type Point = keyof Coord;
 
-let somePoint: Point = 'lat';
+const c: Point = 'lat'; 
 
 // ---------
 
-function log(arg: string | null) {
+function log(arg: string | null): void {
+    arg?.toLowerCase();
+
     if (arg == null) {
-        console.log('...');
+        throw new Error(`${arg} - has null type`);
     } else {
         arg.toUpperCase();
     }
 }
+
+// ---------
+
+const id1: Symbol = Symbol('id');
+const id2: Symbol = Symbol('id');
+
+console.log(id1 == id2);
+
+// ---------
+
+let user = {
+    name: 'John'
+};
+
+let age: Symbol = Symbol('age');
+
+// user[age] = 19;
