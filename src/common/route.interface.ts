ff487eaa70127f7e57856c852a8a583f5d948dc5
obkin <kapellwork@gmail.com>
2023-04-20 14:39:50 +0300

    stage-3 / added: BaseController with intefrace

---
+++ src/common/route.interface.ts
@@ -0,0 +1,7 @@
+import { Request, Response, NextFunction, Router } from "express";
+
+export class IControllerRoute {
+    path: string;
+    func: (req: Request, res: Response, next: NextFunction) => void;
+    method: keyof Pick<Router, 'get' | 'post' | 'delete' | 'patch' | 'put'>;
+}
